7df2257a7511811836bcd94c56bffdb74bb21684
Mosiah Valdez-Bates <mosiahvaldezbates@gmail.com>
2025-12-29 04:32:28 -0700

    testing

--- code/mac_changer.py
+++ code/mac_changer.py
@@ -1,4 +1,10 @@
 import subprocess
 
-subprocess.call('ifconfig', shell=True)
+interface = 'wlan0'
+new_mac = '00:11:22:33:44:55'
+
+subprocess.call(f'ifconfig {interface} down', shell=True)
+subprocess.call(f'ifconfig {interface} hw ether {new_mac}', shell=True)
+subprocess.call(f'ifconfig {interface} up', shell=True)
+
 
